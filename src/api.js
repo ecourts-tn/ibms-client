165bb8d6bb9d522e27f61c752b651f7074a658ca
deena17 <deenadayalan17@gmail.com>
2024-07-22 10:02:27 +0530

    restructed

--- src/api.js
+++ src/api.js
@@ -3,9 +3,9 @@ import { ACCESS_TOKEN } from "./constants";
 
 //const apiUrl = "http://192.168.100.135:8000/";
 
-const apiUrl = "https://1ab7-117-197-189-32.ngrok-free.app/"
+// const apiUrl = "https://1ab7-117-197-189-32.ngrok-free.app/"
 
-// const apiUrl = "http://localhost:8000/"
+const apiUrl = "http://localhost:8000/"
 
 const api = axios.create({
     baseURL: apiUrl
