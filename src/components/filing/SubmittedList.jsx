e5ca8f7ec9dd9c3fe14fa44b401db5c152ac7e08
ecourts-tn <deenadayalan.mhc@gmail.com>
2024-12-18 10:19:03 +0530

    filter

--- src/components/filing/SubmittedList.jsx
+++ src/components/filing/SubmittedList.jsx
@@ -18,6 +18,14 @@ const SubmittedList = () => {
     const {t} = useTranslation()
     const {language} = useContext(LanguageContext)
     const[selectedDocument, setSelectedDocument] = useState(null)
+    const [searchTerm, setSearchTerm] = useState('')
+        
+    // Pagination state
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage, setItemsPerPage] = useState(10) // Default items per page
+    const [totalItems, setTotalItems] = useState(0)
+
+
     const handleShow = (document) => {
         setSelectedDocument(document)
     }
@@ -44,6 +52,7 @@ const SubmittedList = () => {
             try{
                 const response = await submittedPetition()
                 setCases(response)
+                setTotalItems(response.length) 
             }catch(error){
                 console.log(error)
             }finally{
@@ -52,6 +61,40 @@ const SubmittedList = () => {
         }
         fetchPetition()
     }, [])
+
+     // Filter petitions based on search term (efile_number or crime_number/year)
+     const filteredPetitions = cases.filter(item => {
+        const efileMatch = item.petition.efile_number.toLowerCase().includes(searchTerm.toLowerCase());
+        
+        // Concatenate crime number and year into a single string for search
+        const crimeNumberYear = `${item.crime?.fir_number}/${item.crime?.fir_year}`;
+        const crimeMatch = crimeNumberYear.toLowerCase().includes(searchTerm.toLowerCase());
+
+        const caseNumber = `${item.petition.reg_type?.type_name}/${item.petition.reg_number}/${item.petition.reg_year}` ;
+        const caseMatch = caseNumber.toLowerCase().includes(searchTerm.toLowerCase());
+
+        return efileMatch || crimeMatch || caseMatch;
+    })
+
+    // Pagination logic
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentPetitions = filteredPetitions.slice(indexOfFirstItem, indexOfLastItem)
+
+    // Handle page change
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+    // Handle items per page change
+    const handleItemsPerPageChange = (event) => {
+        setItemsPerPage(Number(event.target.value)) // Update items per page
+        setCurrentPage(1) // Reset to the first page whenever the items per page change
+    }
+
+    // Calculate total number of pages
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(filteredPetitions.length / itemsPerPage); i++) {
+        pageNumbers.push(i)
+    }
 
     const handleSubmit = async(efile_no) => {
         try{
@@ -85,6 +128,37 @@ const SubmittedList = () => {
                             </ol>
                         </nav>
                         <h3 className="pb-2"><strong>{t('submitted_petition')}</strong></h3>
+                        <div className="row mb-3">
+                            <label className="mr-2">{t('Search')}:</label>
+                            <div className="col-md-3">
+                                {/* Search Box */}
+                                <div className="d-flex align-items-center">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder={t('Search Case or Efile or Crime number')}
+                                        value={searchTerm}
+                                        onChange={(e) => setSearchTerm(e.target.value)} 
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="col-md-1">
+                                <div className="d-flex align-items-center">
+                                    <label className="mr-2">{t('Filter')}:</label>
+                                    <select 
+                                        className="form-control" 
+                                        value={itemsPerPage} 
+                                        onChange={handleItemsPerPageChange}
+                                    >
+                                        <option value={10}>10</option>
+                                        <option value={15}>15</option>
+                                        <option value={25}>25</option>
+                                        <option value={50}>50</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                         <table className="table table-striped table-bordered">
                             <thead className="bg-secondary">
                                 <tr>
@@ -99,14 +173,33 @@ const SubmittedList = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                { cases.map((item, index) => (
+                                { currentPetitions.map((item, index) => (
                                 <tr>
-                                    <td>{ index+1 }</td>
-                                    <td>
+                                    <td>{ index+1+indexOfFirstItem }</td>
+                                    {/* <td>
                                         <Link to="/filing/detail" state={{efile_no:item.petition.efile_number}}>
                                             <strong>{ item.petition.efile_number }</strong>
                                         </Link>
                                         <span style={{display:'block'}}>{t('efile_date')}: { formatDate(item.petition.efile_date) }</span>
+                                    </td> */}
+                                    <td>
+                                        <Link 
+                                            to="/filing/detail" 
+                                            state={item.petition?.efile_number ? { efile_no: item.petition.efile_number } : undefined}
+                                        >
+                                            {item.petition?.reg_type?.type_name && item.petition?.reg_number && item.petition?.reg_year ? (
+                                                <strong>{`${item.petition.reg_type.type_name}/${item.petition.reg_number}/${item.petition.reg_year}`}</strong>
+                                            ) : null}
+                                            <br />
+                                            {item.petition?.efile_number ? (
+                                                <strong>{item.petition.efile_number}</strong>
+                                            ) : null}
+                                        </Link>
+                                        {item.petition?.efile_date ? (
+                                            <span style={{ display: "block" }}>
+                                                {t('efile_date')}: {formatDate(item.petition.efile_date)}
+                                            </span>
+                                        ) : null}
                                     </td>
                                     <td>
                                         {item.petition.filing_type ? `${item.petition.filing_type?.type_name}/${item.petition.filing_number}/${item.petition.filing_year}` : null}
@@ -167,6 +260,27 @@ const SubmittedList = () => {
                                 ))}
                             </tbody>
                         </table>
+                        {/* Pagination Controls */}
+                        <div className="d-flex justify-content-between mt-3">
+                            <div className="pagination">
+                                <ul className="pagination">
+                                    <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                                        <button className="page-link" onClick={() => paginate(currentPage - 1)}>{t('previous')}</button>
+                                    </li>
+                                    {pageNumbers.map(number => (
+                                        <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+                                            <button className="page-link" onClick={() => paginate(number)}>{number}</button>
+                                        </li>
+                                    ))}
+                                    <li className={`page-item ${currentPage === pageNumbers.length ? 'disabled' : ''}`}>
+                                        <button className="page-link" onClick={() => paginate(currentPage + 1)}>{t('next')}</button>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="page-info">
+                                <span>{t('showing')} {indexOfFirstItem + 1} {t('to')} {indexOfLastItem} {t('of')} {filteredPetitions.length} {t('entries')}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
