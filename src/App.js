e236fc89637a857937c0720efab1b7d9e4582349
ecourts-tn <deenadayalan.mhc@gmail.com>
2025-01-22 12:17:07 +0530

    merge from santhosh

--- src/App.js
+++ src/App.js
@@ -8,20 +8,13 @@ import { AuthProvider } from "contexts/AuthContext";
 import { AppProvider } from 'contexts/AppContext';
 /* -------Authentication -----------*/
 import { publicRoutes } from 'routes/publicRoutes';
-<<<<<<< HEAD
-import { ABailFilingRoutes } from 'routes/abilRoutes';
-=======
 import { filingRoutes } from 'routes/filingRoutes';
->>>>>>> c978c7ed4b47e6ab70c2c25e24a525bdf3c861e7
 import { CourtRoutes } from 'routes/courtRoutes';
 import { policeRoutes } from 'routes/policeRoutes';
 import { prosecutorRoutes } from 'routes/prosecutorRoutes';
 import { prisonRoutes } from 'routes/prisonRoutes';
 import { BailFilingRoutes } from 'routes/bailRoutes';
-<<<<<<< HEAD
-=======
 import { ABailFilingRoutes } from 'routes/abilRoutes';
->>>>>>> c978c7ed4b47e6ab70c2c25e24a525bdf3c861e7
 import { ConditionFilingRoutes } from 'routes/conditionRoutes';
 import { InterveneRoutes } from 'routes/interveneRoute';
 import { ModificationFilingRoutes } from 'routes/modificationRoutes';
@@ -31,7 +24,7 @@ import { ExtensionRoutes } from 'routes/extensionRoutes';
 import { ReturnPassportRoutes } from 'routes/passportRoutes';
 import { ReturnPropertyRoutes } from 'routes/propertyRoutes';
 import DepartmentRegistration from 'components/auth/DepartmentRegistration';
-import { filingRoutes } from 'routes/filingRoutes';
+
 
 function App() {
 
